f4e4dd55aa9f3140ef38c82cb17c3f69b0d657ce
ChrisMwangi1 <wanjikuchristine201@gmail.com>
2024-01-13 01:08:52 -0800

    More .c files

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,22 @@
+include "main.h"
+
+/**
+ * memcpy - Copies a memory adress.
+ * @dest: memory stored adress
+ * @src: memory copied adress
+ * @n: bytes number
+ * Return: pointer storing the adress of the destination
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int x;
+	int y = n;
+
+	for (x = 0; x < i; x++);
+	{
+		dest[x] = src[x];
+		n--;
+	}
+	return (dest);
+}
