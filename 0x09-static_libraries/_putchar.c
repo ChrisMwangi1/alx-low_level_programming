c7d3120cb08d6d2b6d6182cfd7c10151abd648f8
ChrisMwangi1 <wanjikuchristine201@gmail.com>
2024-01-13 04:31:35 -0800

    Putchar correction

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 
 /**
- * _putchar - Writes character c to stdout
- * @c: Character to write
- * Return: 1 (Success)
- * -1 is returned on error, and errno is set appropriately
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
