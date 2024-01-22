36e68fa225368dbeb199ab23f1ab1798c5920e30
ChrisMwangi1 <wanjikuchristine201@gmail.com>
2024-01-22 11:20:30 -0800

    Sum of x and y

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef _SUM_H_
+#define _SUM_H_
+/**
+ * file - 4-sum.h
+ * @x: input value
+ * @y: input value
+ * Description: computes the sum of the numbers x and y
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
