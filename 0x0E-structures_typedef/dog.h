d49fa6b60f8df6b317019d0cba71dbc3b3374ca9
ChrisMwangi1 <wanjikuchristine201@gmail.com>
2024-01-23 01:09:46 -0800

    Initialize the variable of type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,20 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+/**
+ * struct dog - file name
+ * @name: name of dog
+ * @age: age value
+ * @owner: owner of dog
+ * Desc: Defines a type struct dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif
